dbc63f747ff1b557dd2377139161f0f420dba5c2
Abdullah khan <akhan283785@gmail.com>
2024-12-18 01:48:27 +0530

    d

--- src/components/DisplayImage.js
+++ src/components/DisplayImage.js
@@ -4,9 +4,9 @@ const DisplayImage = () => {
   const [showImage, setShowImage] = useState();
   let [count, setCount] = useState(0);
   let imges = [
-    "https://images.pexels.com/photos/29773887/pexels-photo-29773887/free-photo-of-charming-getreidegasse-in-salzburg-austria.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/4863950/pexels-photo-4863950.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/19929417/pexels-photo-19929417/free-photo-of-view-of-a-street-and-buildings-in-merano-south-tyrol-italy.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem/image/MQ6-0-image-20220419101504/01-mercedes-benz-the-new-eqs-suv-x296-2021-1080x1350.jpeg",
+    "https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem_546277676/image/MQ6-0-image-20220819111550/02-mercedes-benz-the-new-eqs-suv-x296-2021-1680x1680.jpeg",
+    "https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem_546277676/image/MQ6-0-image-20220819111550/02-mercedes-benz-the-new-eqs-suv-x296-2021-1680x1680.jpeg",
   ];
 
   useEffect(() => {
